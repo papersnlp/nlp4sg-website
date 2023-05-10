2a65641739740cc04d5ad2ed951b0c6454f34c1c
feradauto <fer.adauto@gmail.com>
2023-05-10 23:56:09 +0200

    fix main page

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -33,8 +33,8 @@ const Card = styled('li', {
   bc: '$contrast6',
   borderRadius: '$3',
   display: 'inline',
-  zIndex:0,
-  opacity:0.7
+  zIndex: 0,
+  opacity: 0.7
 });
 const Home = (props) => {
   const router = useRouter();
@@ -47,9 +47,9 @@ const Home = (props) => {
         <Box
           column
           center
-          css={{ width: '100vw', height: '96vh', backgroundColor: '$contrast1', zIndex: 0 }}
+          css={{ width: '100vw', backgroundColor: '$contrast1', position: 'relative', zIndex: 0 }}
         >
-          <Box container column center>
+          <Box container column css={{ py: '$6' }}>
             <EarthIcon />
             <Text
               type="title"
@@ -74,41 +74,42 @@ const Home = (props) => {
             >
               {content.index.slogan}
             </Text>
-          </Box>
-          <Box container css={{ pt: '0px', pb: '$5', gap: '$3' }}>
-            <Grid css={{ gridTemplateColumns: 'repeat(auto-fit, minmax($7, 1fr))' }}>
-            {content.index.content.map((b, c) => {
-              if (b.type === 'visualization'){
-                return(
-                b['content'].map((v, k) => {
-                  return(
-                  <Card key={k}>
-                     
 
-                    <Link
-                      href={v.path}
-                      /* onClick={() => router.push(v.path)} */
-                      css={{
-                        backgroundImage:"url("+v.img+")",
-                        backgroundSize:'cover',
-                        height: '200px',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-end',
-                      }}
-                    >
-                    </Link>
+          
+            <Grid>
+              {content.index.content.map((b, c) => {
+                if (b.type === 'visualization') {
+                  return (
+                    b['content'].map((v, k) => {
+                      return (
+                        <Card key={k}>
 
-                    <Box css={{ flex: 1 }}></Box>
-                <Box css={{ borderTop: '1.5px solid $contrast5', p: '$4', py: '$1' }}>
-                  <Text>{v.linkText}</Text>
-                </Box>
-                  </Card>
-                  );
-              }));}
-            }
+
+                          <Link
+                            href={v.path}
+                            /* onClick={() => router.push(v.path)} */
+                            css={{
+                              backgroundImage: "url(" + v.img + ")",
+                              backgroundSize: 'cover',
+                              height: '200px',
+                              display: 'flex',
+                              flexDirection: 'column',
+                              justifyContent: 'flex-end',
+                            }}
+                          >
+                          </Link>
+
+                          <Box css={{ flex: 1 }}></Box>
+                          <Box css={{ borderTop: '1.5px solid $contrast5', p: '$4', py: '$1' }}>
+                            <Text>{v.linkText}</Text>
+                          </Box>
+                        </Card>
+                      );
+                    }));
+                }
+              }
               )}
-              </Grid>
+            </Grid>
 
 
           </Box>
